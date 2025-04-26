ff60e297933d6b08bfd0c5d1cbc3138dd21cb3cc
SarahHuewels <130989147+SarahHuewels@users.noreply.github.com>
2025-04-26 11:18:09 +0200

    finish multiple attention heads class

--- default_transformer.py
+++ default_transformer.py
@@ -39,6 +39,7 @@ def scaled_dot_product_attention(query, key, value):
     return torch.bmm(weights, value)
 
 # Multihead attention: having several heads allows the model to focus on several aspects at once
+# Single Head attention layer:
 class AttentionHead(nn.Module):
     def __init__(self, embed_dim, head_dim) -> None:
         super().__init__()
@@ -50,3 +51,22 @@ class AttentionHead(nn.Module):
     def forward(self, hidden_state):
         attn_outputs = scaled_dot_product_attention(self.q(hidden_state), self.k(hidden_state), self.v(hidden_state))
         return(attn_outputs)
+
+# Multihead attention layer:
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        embed_dim = config.hidden_size
+        num_heads = config.num_attention_heads
+        head_dim = embed_dim // num_heads
+        self.heads = nn.ModuleList([AttentionHead(embed_dim, head_dim) for _ in range(num_heads)])
+        self.output_linear = nn.Linear(embed_dim, embed_dim)
+    
+    def forward(self, hidden_state):
+        x = torch.cat([h(hidden_state) for h in self.heads], dim=-1)
+        x = self.output_linear(x)
+        return x
+
+multihead_attn = MultiHeadAttention(config)
+attn_output = multihead_attn(input_embeds)
+print(attn_output.size())
