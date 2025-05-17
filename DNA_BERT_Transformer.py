a4b8db5c44e3b585f69bf088b343523a438e9dfb
SarahHuewels <130989147+SarahHuewels@users.noreply.github.com>
2025-05-17 12:01:16 +0200

    create sbatch files for running toy model and final model on cluster

--- DNA_BERT_Transformer.py
+++ DNA_BERT_Transformer.py
@@ -72,8 +72,13 @@ def train_model(model, dataloader, dna_tokenizer, id_to_base, epochs=3, lr=2e-5,
 
     model.train()
     for epoch in range(epochs):
+        counter = 0
         total_loss = 0
         for batch in dataloader:
+            counter += 1
+            print("Round", counter)
+            if counter == 10:
+                break
             sample_seqs, ref_seqs, phenotypes, exon_masks, rep_masks, positions, chroms = batch
             # sample_seqs: [batch_size, chunk_size]
             # phenotypes: [batch_size, num_phenotypes]
